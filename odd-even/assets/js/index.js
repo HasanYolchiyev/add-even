1fdd54b44462c733a1f759ada237338e26ef8b74
HasanYolchiyev <frontendhy2008@gmail.com>
2023-08-04 23:27:12 +0400

    initial commit

---
+++ odd-even/assets/js/index.js
@@ -0,0 +1,9 @@
+const number = prompt("regem daxil edin: ");
+if(number % 2 == 0) {
+    console.log("bu eded cutdur");
+    //?meselen ekrana 6 ededini yazsaniz "Console" da "bu eded cutdur yazisini goreceksiniz"
+}
+else {
+    console.log("bu eded tekdir");   
+    //?meselen ekrana 5 ededini yazsaniz "Console" da "bu eded tekdir yazisini goreceksiniz"
+}
